6e6afdc958127f3acd0fba5f7a8b107f34052466
geraldmaale <55647505+geraldmaale@users.noreply.github.com>
2021-08-20 15:27:15 +0800

    Modified content accessed via MSAL

--- BlazorWasm.B2C/Server/Startup.cs
+++ BlazorWasm.B2C/Server/Startup.cs
@@ -1,26 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using BlazorMsIdentity.Shared;
-using BlazorMsIdentity.Shared.Services;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorServer.B2C.Data;
-using Microsoft.Graph;
-using Skclusive.Material.Component;
-using Skclusive.Material.Core;
+using Microsoft.Identity.Web;
+using System.Linq;
 
-namespace BlazorServer.B2C
+namespace BlazorWasm.B2C.Server
 {
     public class Startup
     {
@@ -35,32 +25,11 @@ namespace BlazorServer.B2C
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // Azure AD Settings
-            services.Configure<AzureAdOptions>(Configuration.GetSection(AzureAdOptions.AzureAdB2C));
-            
-            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(Configuration.GetSection(AzureAdOptions.AzureAdB2C));
-            services.AddControllersWithViews()
-                .AddMicrosoftIdentityUI();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAd"));
 
-            services.AddAuthorization(options =>
-            {
-                // By default, all incoming requests will be authorized according to the default policy
-                options.FallbackPolicy = options.DefaultPolicy;
-            });
-
+            services.AddControllersWithViews();
             services.AddRazorPages();
-            services.AddServerSideBlazor()
-                .AddMicrosoftIdentityConsentHandler();
-            
-            services.AddSingleton<WeatherForecastService>();
-
-            services.AddSingleton<GraphServiceClient>();
-            
-            // Register Policies
-            services.RegisterPolicies();
-            
-            services.TryAddMaterialServices(new MaterialConfigBuilder().Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,6 +38,7 @@ namespace BlazorServer.B2C
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
             }
             else
             {
@@ -78,6 +48,7 @@ namespace BlazorServer.B2C
             }
 
             app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -87,9 +58,9 @@ namespace BlazorServer.B2C
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
